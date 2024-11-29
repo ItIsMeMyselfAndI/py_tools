dbe180fbcf7015c39d2576b0cf083a5819e0cf9b
phan2mghost <phan2mghost@phan2mghost.phan2mghost>
2024-11-30 01:04:49 +0800

    added execute and shell feature for nethack2.py

--- nethack2.py
+++ nethack2.py
@@ -9,6 +9,7 @@ import threading
 class NetHack:
     def __init__(self):
         self.manual()
+        self.host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def manual(self):
         parser = argparse.ArgumentParser(
@@ -17,10 +18,10 @@ class NetHack:
             epilog=textwrap.dedent(
                 """
                 Examples:
-                (1) ./nethack2.py -t <ip> -p <port> -l -s
-                \t\t-> Listen on <ip>:<port> and open shell.
-                (2) ./nethack2.py -t <ip> -p <port> -l -e <command>
+                (1) ./nethack2.py -t <ip> -p <port> -l -e <command>
                 \t\t-> Listen on <ip>:<port> and execute command.
+                (2) ./nethack2.py -t <ip> -p <port> -l -s
+                \t\t-> Listen on <ip>:<port> and open shell.
                 (3) ./nethack2.py -t <ip> -p <port> -l -u <filename>
                 \t\t-> Listen on <ip>:<port> and upload <filename>.
                 (4) ./nethack2.py -t <ip> -p <port>
@@ -46,14 +47,14 @@ class NetHack:
             help="Listen" 
         )
         parser.add_argument(
-            "-s", "--shell",
-            action="store_true",
-            help="Open shell"
-        )
-        parser.add_argument(
             "-e", "--execute",
             action="store", type=str,
             help="Execute command"
+        )
+        parser.add_argument(
+            "-s", "--shell",
+            action="store_true",
+            help="Open shell"
         )
         parser.add_argument(
             "-u", "--upload",
@@ -63,9 +64,75 @@ class NetHack:
         )
         self.args = parser.parse_args()
 
+    def createServer(self):
+        self.host.bind((self.args.target, self.args.port))
+        self.host.listen(5)
+        print(f"[*] Server is listening on {self.args.target}:{self.args.port}")
+        while True:
+            client, conn = self.host.accept()
+            thread = threading.Thread(
+                target=self._clientHandler, args=(client,)
+            )
+            thread.start()
+
+    def _clientHandler(self, client):
+        client.send("[*] Connected to the server.\n".encode())
+        if self.args.execute:
+            client.send("[*] Press enter to see output.")
+            output = self.execute()
+            client.send(output)
+            client.close()
+        elif self.args.shell:
+            self.shell(client)
+        elif self.args.upload:
+            pass
+
+    def _recvData(self, host):
+        buffer = b""
+        length = 2048
+        while True:
+            data = host.recv(length)
+            buffer += data
+            if len(data) < length:
+                break
+        return buffer
+
+    def execute(self, cmd=None):
+        if cmd == None:
+            cmd = self.args.execute
+        print(f"[*] Execute \"{cmd}\"")
+        output = subprocess.check_output(
+            shlex.split(cmd),
+            stderr=subprocess.STDOUT
+        )
+        return output
+
+    def shell(self, host):
+        while True:
+            host.send("NetHack ~> ".encode())
+            cmd = self._recvData(host).decode()
+            output = self.execute(cmd)
+            host.send(output)
+
+    def createClient(self):
+        self.host.connect((self.args.target, self.args.port))
+        while True:
+            data = self._recvData(self.host)
+            print(data.decode(), end="")
+            cmd = input()
+            self.host.send(cmd.encode())
+    
+    def run(self):
+        if self.args.listen:
+            self.createServer()
+        else:
+            self.createClient()
+
+
+
 def main():
     if __name__ == "__main__":
         nethack = NetHack()
-        nethack.manual()
+        nethack.run()
 
 main()
